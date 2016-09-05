20a4196a8a6b7570015b6779b3964de644a35ec9
Anton <2hammer90@mail.ru>
2016-09-06 00:07:29 +0300

    fixed route in edit page

--- src/app/pages/courses/edit/edit-course.component.ts
+++ src/app/pages/courses/edit/edit-course.component.ts
@@ -1,27 +1,35 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit, OnDestroy} from '@angular/core'
 import {CoursesService} from "../../../services/courses.service";
 import {ActivatedRoute} from "@angular/router";
 import {Location} from "@angular/common";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'home',
     template: require('./edit-course.html'),
 })
-export class EditCourseComponent implements OnInit {
+export class EditCourseComponent implements OnInit, OnDestroy {
     course: Object;
     id: string = '';
+    routeSub: Subscription;
 
-    constructor(
-        private route: ActivatedRoute,
-        private coursesService: CoursesService,
-        public location: Location
-    ) {
-        route.params.subscribe(params => {
-            this.id = params['id'];
-        });
+    constructor(private route: ActivatedRoute,
+                private coursesService: CoursesService,
+                private location: Location) {
     }
 
     ngOnInit(): void {
+        this.routeSub = this.route.params.subscribe(params => {
+            this.id = params['id'];
+            this.getCourse();
+        });
+    }
+
+    ngOnDestroy() {
+        this.routeSub.unsubscribe();
+    }
+
+    getCourse() {
         this.coursesService
             .getCourse(this.id)
             .subscribe(res => this.renderCourse(res))
